0f0a0390f2a95ab0a447d1b4e8a192ee8405d174
Aqmal <50930584+kifasu@users.noreply.github.com>
2023-10-24 16:08:12 +0800

    (remove) playground route and container

--- src/navigations/Routes.tsx
+++ src/navigations/Routes.tsx
@@ -2,17 +2,14 @@ import { FunctionComponent } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import HomePage from 'containers/Home';
-import PlaygroundPage from 'containers/Playground';
 import BranchNameGeneratorPage from 'containers/GenerateBranchName';
 
 const NavRoutes: FunctionComponent = () => {
-  const enableTest = true;
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path='/generate-branch-name' element={<BranchNameGeneratorPage />} />
-      {enableTest && <Route path="/pg" element={<PlaygroundPage />} />}
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
